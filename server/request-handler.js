1962773a6f10713ca1610d4545e5fcedb4300174
francisngo <francis.t.ngo@gmail.com>
2017-07-25 12:02:38 -0700

    create GET and POST method and send object containing results to server

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,24 +11,17 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+exports.requestHandler = function(request, response) {
+  // These headers will allow Cross-Origin Resource Sharing (CORS).
+  // This code allows this server to talk to websites that
+  // are on different domains, for instance, your chat client.
+  //
+  // Your chat client is running from a url like file://your/chat/client/index.html,
+  // which is considered a different domain.
+  //
+  // Another way to get around this restriction is to serve you chat
+  // client from this domain by setting up static file serving.
 
-
-var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -43,23 +36,14 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -68,5 +52,54 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+
+  var results = [];
+
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  // The outgoing status.
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
+
+  response.writeHead(statusCode, headers);
+
+  if (request.method === 'GET') {
+    //getting data from server
+    //error
+    request.on('error', (error) => {
+      statusCode = 400;
+      response.writeHead(statusCode);
+      response.end();
+    });
+    //send back all result
+    response.end(JSON.stringify({results}));
+
+  } else if (request.method === 'POST') {
+    //sending data to server
+    request.on('data', (chunk) => {
+      results.push(JSON.parse(chunk));
+      statusCode = 200;
+      response.writeHead(statusCode);
+    });
+
+    request.on('error', (error) => {
+      console.error('error');
+      statusCode = 400;
+      response.writeHead(statusCode);
+      //nothing to return
+      response.end();
+    });
+
+    request.on('end', () => {
+      response.end();
+    });
+  }
 };
